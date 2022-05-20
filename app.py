9bbef8904e1ab44242d4e00797bb3cf431f6ee59
Pankaj Desai <1480pankajdesai@gmail.com>
2022-05-20 10:39:12 +0530

    Camera application using flask and python

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+
+# Function to read and capture the camera frame
+def generate_frame():
+    while True:
+        success, frame = camera.read()
+        if success:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+        else:
+            break
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/video")
+def video():
+    return Response(
+        generate_frame(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
